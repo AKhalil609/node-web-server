9128c579173c4cff31313053ac403fc97acdfdcd
Ahmed Khalil <ahmed.hegab28@gmail.com>
2018-05-10 05:36:59 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) =>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) =>{
+        if(err){
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintanace.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle : 'Home',
+        welcomeMessage : 'Welcome to my website'
+    });
+});
+
+app.get('/about', (req, res)=>{
+    res.render('about.hbs',{
+        pageTitle : 'About',
+        welcomeMessage: 'Welcome to about!'
+    });
+});
+app.listen(3000);
